ee038e25cd92e137d5ee258fee9a542db6f1897a
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-01-10 18:28:50 +0000

    Redux config, added basic routes

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+const App = () => {
+  const mode = useSelector((state) => state.mode);//grabbing value created in initial state
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> {/*resets css to basic css*/}
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  )
+}
+export default App
